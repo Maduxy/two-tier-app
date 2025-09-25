630d6b74f774aacbfe0734a46348ccf340bab7ca
Madu <onyejiakamadu@gmail.com>
2025-09-25 15:14:22 +0100

    Updated API URL to point to backend EC2

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,7 +4,7 @@ function Data() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://34.245.32.196:5000/data")
+    fetch("http://54.229.118.25:5000/data")
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
